4e90db7f00f142f75617857779d67aadf582cb97
Akexorcist <akexorcist@gmail.com>
2023-08-16 22:49:18 +0700

    Add core-kts in :base:ui module

--- base/ui/build.gradle.kts
+++ base/ui/build.gradle.kts
@@ -32,6 +32,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.10.1")
+
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
