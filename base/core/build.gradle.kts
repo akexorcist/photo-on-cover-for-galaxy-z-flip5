d5611ea3833e33ed854d980e4cf1e3d48a72db00
Akexorcist <akexorcist@gmail.com>
2023-08-16 22:47:14 +0700

    Enable minify and set version to 1.0.0

--- base/core/build.gradle.kts
+++ base/core/build.gradle.kts
@@ -19,8 +19,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = true
         }
     }
     compileOptions {
