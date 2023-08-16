8186502ed67ef52712b77ed8d061031a03e3b179
Akexorcist <akexorcist@gmail.com>
2023-08-16 14:31:21 +0700

    Force home screen use rounded corner rectangle for photo

--- app/src/main/java/com/akexorcist/photooncover/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/akexorcist/photooncover/ui/feature/home/HomeScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.FabPosition
@@ -467,7 +468,7 @@ private fun PhotoItem(
             modifier = Modifier
                 .fillMaxSize()
                 .aspectRatio(ratio = PhotoRatioForGalaxyZFlip5)
-                .clip(MaterialTheme.shapes.large),
+                .clip(RoundedCornerShape(16.dp)),
             model = ImageRequest.Builder(LocalContext.current)
                 .data(photoFile)
                 .crossfade(300)
